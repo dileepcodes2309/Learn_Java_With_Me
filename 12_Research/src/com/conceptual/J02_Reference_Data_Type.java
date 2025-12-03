5f8512060e15a832e17967c01856db3f04ca2106
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-04 04:02:47 +0530

    reference type

---
+++ 12_Research/src/com/conceptual/J02_Reference_Data_Type.java
@@ -0,0 +1,16 @@
+package com.conceptual;
+
+public class J02_Reference_Data_Type {
+
+    public static void main(String[] args) {
+        String referenceType = """
+                A reference type is a variable that holds the address (or pointer)
+                of an object that is stored in heap memory.
+                """;
+
+        System.out.println(referenceType);
+
+        String s = new String("Dileep");
+
+    }
+}
